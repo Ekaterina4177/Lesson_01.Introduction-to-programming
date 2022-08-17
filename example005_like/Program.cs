4c321e68911bbb42f9ba05941d4634e620444cd8
Ekaterina <you@example.com>
2022-08-17 18:53:31 +0300

    орфография

--- example005_like/Program.cs
+++ example005_like/Program.cs
@@ -1,1 +1,12 @@
-﻿c
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша");
+{
+    Console.WriteLine(Ура, это же Маша!);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
